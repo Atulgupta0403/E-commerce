37154c2be464f24a55b3a4b31824eba6c3165761
atulgupta0403 <atulgupta0403@gmail.com>
2024-06-15 13:23:45 +0530

    login setup

--- Pages/login.jsx
+++ Pages/login.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./CSS/login.css"
+import Footer from "../Components/footer/footer"
 
 const login = () => {
   return (
-    <div>
-      
+    <div className='login'>
+      <div className="login-container">
+        <h1>Sign up</h1>
+        <div className="login-fields">
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email Address'/>
+          <input type="password" placeholder='Password'/>
+        </div>
+        <button>Continue</button>
+        <p>Already have an Account ? <span>Login now</span></p>
+
+        <div className="login-agree">
+          <input type="checkbox" />
+          <p>By contuning , i agree to the terms of use & privacy policy .</p>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
